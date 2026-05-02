6dfcd3262802adc0d912a258f7e29517a5ac23de
DuyLinh0212 <tienben9123@gmail.com>
2026-05-03 00:16:46 +0700

    Update training config (lr, momentum)

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 config = {
     'max_epoch' : 50,
     'log_train' : 100,
-    'lr' : 1e-4,
+    'lr' : 1e-3,
     'starting_epoch' : 0,
     'batch_size' : 8,
     'log_val' : 10,
     'task' : 'acl', # "meniscus" and  "acl" are the other options
     'weight_decay' : 1e-4,
-    'momentum' : 0.7,
+    'momentum' : 0.9,
     'patience' : 5,
     'save_model' : 1,
     'exp_name' : 'test',
